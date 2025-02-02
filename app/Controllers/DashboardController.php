7b031b61601cb73419e0a0931684d483f83c2271
Abdullah Al Mamun <mamun167359@gmail.com>
2025-02-02 20:28:22 +0600

    feat: add enum

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Auth;
+use App\Enums\AttendeeFieldsEnum;
+use App\Enums\EventFieldsEnum;
 use App\Models\Attendee;
 use App\Models\Event;
 use App\View;
@@ -11,8 +13,8 @@ class DashboardController
 {
     public function dashboard()
     {
-        $totalEvents = (new Event())->where('user_id', Auth::id())->getTotalCount();
-        $totalAttendees = (new Attendee())->where('user_id', Auth::id())->getTotalCount();
+        $totalEvents = (new Event())->where(EventFieldsEnum::USER_ID->value, Auth::id())->getTotalCount();
+        $totalAttendees = (new Attendee())->where(AttendeeFieldsEnum::USER_ID->value, Auth::id())->getTotalCount();
         View::renderAndEcho('dashboard.home', [
             'totalEvents'    => $totalEvents,
             'totalAttendees' => $totalAttendees
